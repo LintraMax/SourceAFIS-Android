1befe6ab1a5a290b38223232740cfa75351fe30a
wilson-ong <3osJKGnEPIjAi9lcEEtg>
2020-05-13 14:26:51 +0800

    Add fingerprintio package to fix Java8 compatibility issue.

--- library/src/main/java/com/machinezoo/sourceafis/ImmutableTemplate.java
+++ library/src/main/java/com/machinezoo/sourceafis/ImmutableTemplate.java
@@ -1,8 +1,11 @@
 // Part of SourceAFIS for Java: https://sourceafis.machinezoo.com/java
 package com.machinezoo.sourceafis;
 
-import static java.util.stream.Collectors.*;
-import java.util.*;
+import java.util.Comparator;
+import java8.util.Comparators;
+import java8.util.J8Arrays;
+import java8.util.stream.Collectors;
+import java8.util.stream.StreamSupport;
 
 class ImmutableTemplate {
 	static final ImmutableTemplate EMPTY = new ImmutableTemplate();
@@ -17,6 +20,7 @@ class ImmutableTemplate {
 	private static final int PRIME = 1610612741;
 	ImmutableTemplate(MutableTemplate mutable) {
 		size = mutable.size;
+		/* src:
 		minutiae = mutable.minutiae.stream()
 			.map(ImmutableMinutia::new)
 			.sorted(Comparator
@@ -26,6 +30,17 @@ class ImmutableTemplate {
 				.thenComparing(m -> m.direction)
 				.thenComparing(m -> m.type))
 			.toArray(ImmutableMinutia[]::new);
+		 */
+		Comparator<ImmutableMinutia> comparator;
+		comparator = Comparators.comparingInt((ImmutableMinutia m) -> ((m.position.x * PRIME) + m.position.y) * PRIME);
+		comparator = Comparators.thenComparing(comparator, m -> m.position.x);
+		comparator = Comparators.thenComparing(comparator, m -> m.position.y);
+		comparator = Comparators.thenComparing(comparator, m -> m.direction);
+		comparator = Comparators.thenComparing(comparator, m -> m.type);
+		minutiae = StreamSupport.stream(mutable.minutiae)
+				.map(ImmutableMinutia::new)
+				.sorted(comparator)
+				.toArray(ImmutableMinutia[]::new);
 		// https://sourceafis.machinezoo.com/transparency/shuffled-minutiae
 		FingerprintTransparency.current().log("shuffled-minutiae", this::mutable);
 		edges = NeighborEdge.buildTable(minutiae);
@@ -33,7 +48,7 @@ class ImmutableTemplate {
 	MutableTemplate mutable() {
 		MutableTemplate mutable = new MutableTemplate();
 		mutable.size = size;
-		mutable.minutiae = Arrays.stream(minutiae).map(ImmutableMinutia::mutable).collect(toList());
+		mutable.minutiae = J8Arrays.stream(minutiae).map(ImmutableMinutia::mutable).collect(Collectors.toList());
 		return mutable;
 	}
 }
